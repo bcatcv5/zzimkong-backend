0130be2ac1d5322e2a6a0f203c6120785c29f6fb
SangBeom-Hahn <hsb990917@gmail.com>
2024-02-08 23:40:46 +0900

    chore: 백엔드 개발환경 세팅 설정
    #4

---
+++ src/test/java/com/boostcamp/zzimkong/ZzimkongBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boostcamp.zzimkong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzimkongBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
