52b9a93b4b533873acc987d00b3a61100ba45b79
SangBeom-Hahn <hsb990917@gmail.com>
2024-02-29 22:32:34 +0900

    fix: 영상 재생 최소 시간 추가
    #6

--- src/main/java/com/boostcamp/zzimkong/support/file/GCPFileUploader.java
+++ src/main/java/com/boostcamp/zzimkong/support/file/GCPFileUploader.java
@@ -32,7 +32,8 @@ public class GCPFileUploader {
 
     private final String bucket;
 
-    private static final int LIMIT_TIME = 600;
+    private static final int MAX = 600;
+    private static final int MIN = 120;
     private static final int CHUNK_SIZE = 1024 * 1024 * 200;
     private static final int START_IDX = 0;
     private static final int DEST_POS = 0;
@@ -133,7 +134,7 @@ public class GCPFileUploader {
         try {
             double videoDuration = getVideoDuration(videoStream);
 
-            if (videoDuration >= LIMIT_TIME) {
+            if (isNotInRange(videoDuration)) {
                 throw new InvalidDurationException();
             }
         } catch (ImageProcessingException e) {
@@ -143,6 +144,10 @@ public class GCPFileUploader {
         } catch (MetadataException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static boolean isNotInRange(double videoDuration) {
+        return videoDuration >= MAX || videoDuration <= MIN;
     }
 
     private static double getVideoDuration(InputStream videoStream) throws ImageProcessingException, IOException, MetadataException {
