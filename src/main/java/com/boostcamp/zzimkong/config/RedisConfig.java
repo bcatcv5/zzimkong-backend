b5e66d2e6d4afafe1e971e20f3585339a6e8f6a9
SangBeom-Hahn <hsb990917@gmail.com>
2024-03-10 13:39:00 +0900

    feat: redis 클라이언트 환경 셋팅
    #10

---
+++ src/main/java/com/boostcamp/zzimkong/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package com.boostcamp.zzimkong.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.redis.data.host}")
+    private String host;
+
+    @Value("${spring.redis.data.port}")
+    private int port;
+
+    @Value("${spring.redis.data.password}")
+    private String password;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(host);
+        redisConfig.setPort(port);
+        redisConfig.setPassword(password);
+
+        return new LettuceConnectionFactory(redisConfig);
+    }
+
+    @Bean
+    public StringRedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        return new StringRedisTemplate(redisConnectionFactory);
+    }
+}
