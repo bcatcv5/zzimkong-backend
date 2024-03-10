9d0976e3ce393bc5a8c17696068242ca514e33bc
SangBeom-Hahn <hsb990917@gmail.com>
2024-03-10 16:02:02 +0900

    feat: MessageConsumer 기능 구현
    #10

---
+++ src/main/java/com/boostcamp/zzimkong/support/file/MessageConsumer.java
@@ -0,0 +1,36 @@
+package com.boostcamp.zzimkong.support.file;
+
+import com.boostcamp.zzimkong.domain.message.Message;
+import com.boostcamp.zzimkong.repository.MessageRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConsumer {
+
+    private final MessageRepository messageRepository;
+    private final RedisTemplate<String, String> redisTemplate;
+    private final ObjectMapper objectMapper;
+
+    public MessageConsumer(MessageRepository messageRepository, RedisTemplate<String, String> redisTemplate, ObjectMapper objectMapper) {
+        this.messageRepository = messageRepository;
+        this.redisTemplate = redisTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public void sendSpaceMessage(String storeFileUrl) {
+        //TODO: 추론 테스트
+        Message saveMessage = messageRepository.save(Message.of("공간", "1709803553327.mp4"));
+        messageRepository.save(saveMessage);
+
+        String key = saveMessage.isSpace() ? "space" : "furniture";
+        try {
+            String value = objectMapper.writeValueAsString(saveMessage);
+            redisTemplate.opsForList().rightPush(key, value);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
