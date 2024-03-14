9a5e3e8b611bc4e11e713d76a09604e0608e7468
SangBeom-Hahn <hsb990917@gmail.com>
2024-03-14 14:06:01 +0900

    feat: ResultRepository 기능 구현
    #7

--- src/main/java/com/boostcamp/zzimkong/repository/modelresult/FurnitureResultRepository.java
+++ src/main/java/com/boostcamp/zzimkong/repository/modelresult/FurnitureResultRepository.java
@@ -1,7 +1,22 @@
 package com.boostcamp.zzimkong.repository.modelresult;
 
+import com.boostcamp.zzimkong.domain.StatusCode;
 import com.boostcamp.zzimkong.domain.furniture.FurnitureModelResult;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FurnitureResultRepository extends JpaRepository<FurnitureModelResult, Long> {
+
+    @Query("select f from FurnitureModelResult f left join fetch f.user where f.statusPushed = :statusPushed")
+    Page<FurnitureModelResult> findByStatusPushed(@Param("statusPushed") Boolean statusPushed, Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update FurnitureModelResult f set f.statusPushed = true where f.statusPushed = :statusPushed")
+    int updateStatusPushed(@Param("statusPushed") Boolean statusPushed);
 }
--- src/main/java/com/boostcamp/zzimkong/repository/modelresult/SpaceResultRepository.java
+++ src/main/java/com/boostcamp/zzimkong/repository/modelresult/SpaceResultRepository.java
@@ -1,7 +1,20 @@
 package com.boostcamp.zzimkong.repository.modelresult;
 
+import com.boostcamp.zzimkong.domain.furniture.FurnitureModelResult;
 import com.boostcamp.zzimkong.domain.space.SpaceModelResult;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SpaceResultRepository extends JpaRepository<SpaceModelResult, Long> {
+
+    @Query("select f from SpaceModelResult f left join fetch f.user where f.statusPushed = :statusPushed")
+    Page<SpaceModelResult> findByStatusPushed(@Param("statusPushed") Boolean statusPushed, Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update SpaceModelResult f set f.statusPushed = true where f.statusPushed = :statusPushed")
+    int updateStatusPushed(@Param("statusPushed") Boolean statusPushed);
 }
