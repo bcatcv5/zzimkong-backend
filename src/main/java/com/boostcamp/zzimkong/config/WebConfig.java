f3f82bf855c6aa81123c71b46e94e75d0d4bed01
hee000 <sy589610@gmail.com>
2024-03-25 02:34:20 +0900

    feat: react view 포워딩
    #27

--- src/main/java/com/boostcamp/zzimkong/config/WebConfig.java
+++ src/main/java/com/boostcamp/zzimkong/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.boostcamp.zzimkong.support.FileTypeEnumConverter;
 import com.boostcamp.zzimkong.support.ObjectTypeEnumConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new ObjectTypeEnumConverter());
         registry.addConverter(new FileTypeEnumConverter());
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/");
     }
 }
