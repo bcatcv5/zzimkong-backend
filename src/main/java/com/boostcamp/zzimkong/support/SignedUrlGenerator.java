e991a771d889d71a3457a9254af5d5716bee3832
SangBeom-Hahn <hsb990917@gmail.com>
2024-03-28 00:15:28 +0900

    refactor: GCP 설정 상수화
    #29

--- src/main/java/com/boostcamp/zzimkong/support/SignedUrlGenerator.java
+++ src/main/java/com/boostcamp/zzimkong/support/SignedUrlGenerator.java
@@ -5,6 +5,7 @@ import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.FileInputStream;
@@ -15,18 +16,29 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 @Component
 public class SignedUrlGenerator {
-    private final Storage storage;;
 
-    public SignedUrlGenerator(Storage storage) {
+    private static final String CLASS_PATH = "src/main/resources/";
+
+    private final Storage storage;
+    private final String bucket;
+    private final String credentials;
+
+    public SignedUrlGenerator(
+            Storage storage,
+            @Value("${spring.cloud.gcp.storage.bucket}") String bucket,
+            @Value("${spring.cloud.gcp.storage.credentials.location}") String credentials
+    ) {
         this.storage = storage;
+        this.bucket = bucket;
+        this.credentials = credentials;
     }
 
     public String generateSignedUrl(String objectPath) throws IOException {
         URL signedUrl = storage.signUrl(
-                BlobInfo.newBuilder(BlobId.of("버킷 이름", objectPath)).build(),
+                BlobInfo.newBuilder(BlobId.of(bucket, objectPath)).build(),
                 5, TimeUnit.MINUTES, // URL의 유효 시간을 5분으로 설정
                 Storage.SignUrlOption.signWith(ServiceAccountCredentials.fromStream(
-                        new FileInputStream("key.json 경로")))
+                        new FileInputStream(CLASS_PATH + credentials)))
         );
         return signedUrl.toString();
     }
